52f6d0fe80c03143466dc58ca8d2928b19e7d7c3
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-06-30 12:16:28 +0800

    update

--- myDjango/myapp/models.py
+++ myDjango/myapp/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-
-
 class Class(models.Model):
     class_name = models.CharField(max_length=8)
 
 class Student(models.Model):
     stu_name  = models.CharField(max_length=8)
     sc = models.ForeignKey(Class, on_delete=None, default=1)
+
+class Cat_type(models.Model):
+    cat_type_name = models.CharField(max_length=8)
+
+class Cat(models.Model):
+    cat_name = models.CharField(max_length=8)
+    cat_age = models.IntegerField(max_length=8)
+    ct = models.ForeignKey(Cat_type, on_delete=models.SET_NULL, default=1, null=True)   #对应的cat_type 后其值变为None
+
+class Nationality(models.Model):
+    na_name = models.CharField(max_length=8)
+
+class Person(models.Model):
+    pe_name = models.CharField(max_length=8)
+    pn = models.ForeignKey(Nationality, on_delete=models.CASCADE, default=1)   #级联删除
