efada48e0d7b9b6d5bb34c3f49514bc79efa2c2c
yuanjiancheng <yuan.jiancheng@ricequant.com>
2021-07-08 13:37:26 +0800

    update

--- myDjango/one/urls.py
+++ myDjango/one/urls.py
@@ -7,5 +7,9 @@ app_name = "one"  #反向url 起作用
 urlpatterns = [
     path('index/', views.index, name="index"),
     path("getStu/", views.getStu),
+    path("set_cookie/", views.set_cookie),
+    path("get_cookie/", views.get_cookie),
+    path("set_salt_cookie/", views.set_salt_cookie),
+    path("get_salt_cookie/", views.get_salt_cookie),
 ]
 
--- myDjango/one/views.py
+++ myDjango/one/views.py
@@ -14,3 +14,29 @@ def getStu(request):
     # print(reverse("myapp:showClass"))  #/myapp/showClass/
     # return HttpResponseRedirect(reverse("myapp:editStu", args=(2,)))   #args  传参
 
+
+def set_cookie(request):
+     response =  HttpResponse("set_cookie")
+     response.set_cookie("ring", "yuan", max_age=24*60*60, path="/")
+     return response
+
+
+def get_cookie(request):
+    ring = request.COOKIES.get("ring")
+    if ring:
+        return HttpResponse("cookie is {}".format(ring,))
+    return HttpResponse("cookie is not exist")
+
+def set_salt_cookie(request):
+    response = HttpResponse("set_salt_cookie")
+    response.set_signed_cookie("one", "two", salt="hello",)
+    return HttpResponse("set_signed_cookie")
+
+#
+# def get_salt_cookie(request):
+#     try:
+#         one = request.get_signed_cookie(key="one", salt="hello")
+#     # if one:
+#     except Exception as e:
+#         return HttpResponse("salt_cookie is {}".format(one,))
+#     return HttpResponse("获取salt_cookie失败")
