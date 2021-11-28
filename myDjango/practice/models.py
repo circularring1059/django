bb59b47f3eb42053817d36adb4d65ec5262b53fc
原件 <ring@163.com>
2021-11-28 17:32:12 +0800

    update

--- myDjango/practice/models.py
+++ myDjango/practice/models.py
@@ -13,10 +13,16 @@ class Author(models.Model):
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='author_book')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='author_book', related_query_name="auth_book")
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'book'
+
+#
+# class FileFile(models.Model):
+#     file_object = models.FileField(upload_to='/uploads/%Y/%m%d')
+#
+
